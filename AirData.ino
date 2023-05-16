0167673dc6a7b16b5e417a2199ba767ae04b473e
771-8bit <kazuki.fujita8823@gmail.com>
2023-05-16 11:44:08 +0900

    deprecate RTOS

--- AirData.ino
+++ AirData.ino
@@ -1,143 +1,71 @@
-float altitude_m = 0;
-float pressure_hPa = 0;
-float temperature_deg = 0;
-float differential_pressure_Pa = 0;
-float air_speed_ms = 0;
-
-#include <TORICA_SD.h>
-int cs_SD = 2;
-TORICA_SD sd(cs_SD);
-char SD_BUF[256];
-
+#include <TimerTCC0.h>
 #include <Adafruit_DPS310.h>
-Adafruit_DPS310 dps;
-sensors_event_t temp_event, pressure_event;
 #include <math.h>
 
-#include <Seeed_Arduino_FreeRTOS.h>
-#define  ERROR_LED_PIN  LED_BUILTIN
-#define ERROR_LED_LIGHTUP_STATE  LOW
-TaskHandle_t Handle_sendUART;
-TaskHandle_t Handle_readUART;
-TaskHandle_t Handle_flashSD;
-TaskHandle_t Handle_monitorTask;
-void myDelayMsUntil(TickType_t* previousWakeTime, int ms) {
-  // portTICK_PERIOD_US = 1000us = 1ms
-  vTaskDelayUntil(previousWakeTime, (ms * 1000) / portTICK_PERIOD_US);
-}
+Adafruit_DPS310 dps;
+bool timer_100Hz = false;
+
+sensors_event_t temp_event, pressure_event;
+float altitude_progress ;
+float altitude = 0;
+float pressure = 0;
+float temperature = 0;
+
+
 
 void setup() {
-  vNopDelayMS(1000); // prevents usb driver crash on startup, do not omit this
-  SerialUSB.begin(460800);
+  Serial.begin(460800);
   Serial1.begin(460800);
 
-  // Set the led the rtos will blink when we have a fatal rtos error
-  // RTOS also Needs to know if high/low is the state that turns on the led.
-  // Error Blink Codes:
-  //    3 blinks - Fatal Rtos Error, something bad happened. Think really hard about what you just changed.
-  //    2 blinks - Malloc Failed, Happens when you couldn't create a rtos object.
-  //               Probably ran out of heap.
-  //    1 blink  - Stack overflow, Task needs more bytes defined for its stack!
-  //               Use the taskMonitor thread to help gauge how much more you need
-  vSetErrorLed(ERROR_LED_PIN, ERROR_LED_LIGHTUP_STATE);
+  TimerTcc0.initialize(10000);  //10,000us=100Hz
+  TimerTcc0.attachInterrupt(timerIsr);
 
-  sd.begin();
-
-  SerialUSB.println("DPS310");
-  if (! dps.begin_I2C()) {
-    SerialUSB.println("Failed to find DPS");
-    while (1) {
-      Serial1.print("-1,-1,-1,0.0,0.0\n");
-      delay(30);
-    };
+  Serial.println("DPS310");
+  if (! dps.begin_I2C()) {             // Can pass in I2C address here
+    //if (! dps.begin_SPI(DPS310_CS)) {  // If you want to use SPI
+    Serial.println("Failed to find DPS");
+    while (1) yield();
   }
-  SerialUSB.println("DPS OK!");
+
+  Serial.println("DPS OK!");
+
   dps.configurePressure(DPS310_32HZ, DPS310_16SAMPLES);
   dps.configureTemperature(DPS310_32HZ, DPS310_2SAMPLES);
-
-  xTaskCreate(readUART, "readUART", 256, NULL, tskIDLE_PRIORITY + 2, &Handle_readUART);
-  xTaskCreate(sendUART, "sendUART", 256, NULL, tskIDLE_PRIORITY + 1, &Handle_sendUART);
-  xTaskCreate(flashSD, "flashSD", 256, NULL, tskIDLE_PRIORITY, &Handle_flashSD);
-  //xTaskCreate(taskMonitor, "Task Monitor", 256, NULL, tskIDLE_PRIORITY, &Handle_monitorTask);
-
-  vTaskStartScheduler();
 }
 
-// loop never run
+
 void loop() {
-}
-
-char sendUART_BUF[256];
-void sendUART(void* pvParameters) {
-  portTickType xLastWakeTime = xTaskGetTickCount();
-  float altitude_progress;
-  while (true) {
-    // DPS310_32HZ ×3 = 96Hz = 10ms
-    myDelayMsUntil(&xLastWakeTime, 10);
+  if (timer_100Hz) {
+    timer_100Hz = false;
     if (dps.temperatureAvailable() && dps.pressureAvailable()) {
 
       dps.getEvents(&temp_event, &pressure_event);
-      pressure_hPa = pressure_event.pressure;
-      temperature_deg = temp_event.temperature;
+      pressure = pressure_event.pressure;
+      temperature = temp_event.temperature;
 
-      altitude_progress = pow(1013.25 / pressure_hPa, 1 / 5.257);
-      altitude_m = (altitude_progress - 1) * (temperature_deg + 273.15) / 0.0065;
-
-      //ToDo read SDP810
-
-      sprintf(sendUART_BUF, "%d,%.2f,%.2f,%.2f,%.2f,%.2f\n", millis(), pressure_hPa, temperature_deg, altitude_m, differential_pressure_Pa, air_speed_ms);
-      Serial1.print(sendUART_BUF);
+      altitude_progress = pow(1013.25 / pressure, 1 / 5.257);
+      altitude = (altitude_progress -1)* (temperature + 273.15) / 0.0065;
     }
+    char buf[256];
+    sprintf(buf,"%.2f,%.2f,%.2f,%.2f,%.2f\n", pressure, temperature, altitude, 0.0, 0.0);
+    Serial1.print(buf);
+
+    //Serial1.print(%d,%,pressure_event.pressure","temp_event.temperature","altitude",差圧,対気速度\n");
+    //Serial1.print("1013.25,23.4,1.23,4.56,7.89\n");
+    //気圧[hPa],温度[deg],気圧高度[m],差圧[Pa],対気速度[m/ss]
+
+  }
+
+  if (Serial1.available()) {
+    char c = (char)Serial1.read();
+    Serial.write(c);
   }
 }
 
-// C:\Users\XXX\AppData\Local\Arduino15\packages\Seeeduino\hardware\samd\1.x.x\cores\arduino\RingBuffer.h
-const int readUART_BUF_SIZE = 256;
-char readUART_BUF[readUART_BUF_SIZE];
-void readUART(void* pvParameters) {
-  portTickType xLastWakeTime = xTaskGetTickCount();
-  while (true) {
-    // 100HZ = 10ms
-    // 記録できればいいのでリアルタイム性は求めない．
-    //バッファに収まりデータを取りこぼさず，スループットがあればOK
-    myDelayMsUntil(&xLastWakeTime, 10);
-    if (Serial1.available()) {
-      int i = 0;
-      int read_length = Serial1.available();
-      if (read_length >= readUART_BUF_SIZE - 1) {
-        read_length = readUART_BUF_SIZE - 1;
-      }
-      Serial1.readBytes(readUART_BUF, read_length);
-      readUART_BUF[read_length] = '\0';
-      Serial1.write(readUART_BUF, read_length + 1);
-      sd.add_str(readUART_BUF);
-    }
-  }
-}
-
-void flashSD(void* pvParameters) {
-  while (true) {
-    sd.flash();
-  }
-}
-
-//*****************************************************************
-// Task will periodicallt print out useful information about the tasks running
-// Is a useful tool to help figure out stack sizes being used
-//*****************************************************************
-void taskMonitor(void* pvParameters) {
-  int measurement;
-  while (true) {
-    SerialUSB.println("");
-    SerialUSB.println("******************************");
-    SerialUSB.println("[Stacks Free Bytes Remaining] ");
-
-    measurement = uxTaskGetStackHighWaterMark(Handle_readUART);
-    SerialUSB.print("Thread readUART: ");
-    SerialUSB.println(measurement);
-
-    SerialUSB.println("******************************");
-
-    delay(1000);
+void timerIsr() {
+  if (timer_100Hz) {
+    //Serial.println("100Hz overrun");
+  } else {
+    timer_100Hz = true;
   }
 }
