bda36f5237ca32ee9fac5894056142ddee6a3868
771-8bit <kazuki.fujita8823@gmail.com>
2023-05-18 21:08:21 +0900

    Update AirData.ino

--- AirData.ino
+++ AirData.ino
@@ -1,4 +1,3 @@
-#include <TimerTC3.h>
 #include <Adafruit_DPS310.h>
 #include <math.h>
 #include <SensirionI2CSdp.h>
@@ -7,14 +6,16 @@
 #include <SD.h>
 #include <TORICA_SD.h>
 
+#define SerialIN  Serial1
+#define SerialOUT Serial1
+
 SensirionI2CSdp sdp;
-uint16_t error;
+volatile uint16_t error;
 char errorMessage[256];
 
-int cs_SD = 2;
+int cs_SD = 28;
 TORICA_SD sd(cs_SD);
 char SD_BUF[256];
-
 
 Adafruit_DPS310 dps;
 sensors_event_t temp_event, pressure_event;
@@ -31,10 +32,17 @@ float temperature_C = 0;
 char buf[256];
 
 void setup() {
+  Serial1.setFIFOSize(1024);
   Serial.begin(460800);
   Serial1.begin(460800);
 
-  //追加
+  pinMode(16, OUTPUT);
+  pinMode(25, OUTPUT);
+
+
+  //while (!Serial) {}
+
+  Wire.setClock(1000000);
   Wire.begin();
 
   sdp.begin(Wire, SDP8XX_I2C_ADDRESS_0);
@@ -86,44 +94,35 @@ void setup() {
   dps.configurePressure(DPS310_32HZ, DPS310_16SAMPLES);
   dps.configureTemperature(DPS310_32HZ, DPS310_2SAMPLES);
 
-
-  //追加
-  delay(100);
-
-  NVIC_SetPriority((IRQn_Type)TC3_IRQn, 3);
-  TimerTc3.initialize(2000);
-  TimerTc3.attachInterrupt(timerIsr);
-
-  sd.begin();
 }
 
+void setup1() {
 
-void loop() {
 
-  // SDに書き込み
-  sd.flash();
-  delay(100);
+  sd.begin();
 
 }
 
 const int readUART_BUF_SIZE = 256;
-char readUART_BUF[readUART_BUF_SIZE];
-
-void timerIsr() {
-  if (Serial1.available()) {
-    int read_length = Serial1.available();
+char readUART_BUF[256];
+void loop() {
+  while (SerialIN.available()) {
+    pinMode(16, LOW);
+    int read_length = SerialIN.available();
     if (read_length >= readUART_BUF_SIZE - 1) {
       read_length = readUART_BUF_SIZE - 1;
     }
-    Serial1.readBytes(readUART_BUF, read_length);
+    SerialIN.readBytes(readUART_BUF, read_length);
     readUART_BUF[read_length] = '\0';
     sd.add_str(readUART_BUF);
-
+    if (!SerialIN.available()) {
+      delay(1);
+    }
+    pinMode(16, HIGH);
   }
 
-
-/*
   if (dps.temperatureAvailable() && dps.pressureAvailable()) {
+    pinMode(25, LOW);
     dps.getEvents(&temp_event, &pressure_event);
     pressure = pressure_event.pressure;
     temperature = temp_event.temperature;
@@ -132,7 +131,11 @@ void timerIsr() {
     altitude = (altitude_progress - 1) * (temperature + 273.15) / 0.0065;
 
     //追加
-    error = sdp.readMeasurement(differentialPressure_Pa, temperature_C);
+    float _differentialPressure_Pa = 0;
+    float _temperature_C = 0;
+    error = sdp.readMeasurement(_differentialPressure_Pa, _temperature_C);
+    differentialPressure_Pa = _differentialPressure_Pa;
+    temperature_C = _temperature_C;
     if (error) {
       Serial.print("Error trying to execute readMeasurement(): ");
       errorToString(error, errorMessage, 256);
@@ -142,11 +145,13 @@ void timerIsr() {
       airspeed_ms = sqrt(abs(2.0 * differentialPressure_Pa / rho_kgm3));
 
     }
-*/
     sprintf(buf, "%.2f,%.2f,%.2f,%.2f,%.2f\n", pressure, temperature, altitude, differentialPressure_Pa , airspeed_ms);
-    Serial1.print(buf);
-
-
+    SerialOUT.print(buf);
+    pinMode(25, HIGH);
   }
+}
 
+void loop1() {
+  sd.flash();
+  delay(10);
 }
